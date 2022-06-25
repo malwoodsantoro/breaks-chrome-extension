b89252a8e9aa65ff585e1d79a9e663457a1837f7
mal-wood <mallorywood413@gmail.com>
2022-06-25 19:07:17 -0400

    clean up

---
+++ src/Form.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import WindowDimensions from './WindowDimensions';
+import { createTheme } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+
+interface Values {
+  name: string,
+  minWidth: string,
+  maxWidth: string
+}
+
+type valuesType = Values[];
+
+export default function Form() {
+
+  const [formValues, setFormValues] = useState<valuesType>([{ name: '', minWidth: '', maxWidth: '' }])
+
+  const onChange = (index: number, e:React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>): void => {
+    let newFormValues: valuesType = [...formValues];
+    const { name, value } = e.currentTarget;
+    newFormValues[index][name as keyof Values] = value;
+    setFormValues(newFormValues);
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    setFormValues(formValues);
+    e.preventDefault();
+  }
+
+  const addFormFields = () => {
+    setFormValues([...formValues, { name: "", minWidth: "", maxWidth: ""}])
+  }
+
+  const removeFormFields = (index: number) => {
+    let newFormValues = [...formValues]
+    newFormValues.splice(index, 1);
+    setFormValues(newFormValues);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        {formValues.map((element, index) => (
+          <div className="form-inline" key={index}>
+            <TextField name="name" id="name" value={element.name || ""} label="Name" variant="standard" onChange={e => onChange(index, e)}/>
+            <TextField name="minWidth" id="minWidth" value={element.minWidth || ""} label="Min-width" variant="standard" onChange={e => onChange(index, e)}/>
+            <TextField name="maxWidth" id="maxWidth" value={element.maxWidth || ""} label="Max-width" variant="standard" onChange={e => onChange(index, e)}/>
+            {
+              index ?
+                <Button variant="outlined" style={{ color: 'red' }} className="button remove" onClick={() => removeFormFields(index)}>Remove</Button>
+                : null
+            }
+          </div>
+        ))}
+        <div className="button-section">
+          <Button variant="text" className="button add" type="button" onClick={() => addFormFields()}>Add Row</Button>
+          <Button variant="outlined" className="button submit" type="submit">Submit</Button>
+        </div>
+      </form>
+      <WindowDimensions values={formValues} />
+    </div>
+  );
+}
